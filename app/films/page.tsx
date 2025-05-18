9feb24908f8f68a198bb5bdd7172994115036c9d
gadji <135466448+Milkjf@users.noreply.github.com>
2025-05-18 19:31:32 +0000

    modification de mon site

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -128,7 +128,9 @@ export default function FilmsPage() {
               className="pl-10"
               autoFocus
             />
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+            <Search
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
+            />
           </div>
           <div className="flex flex-col sm:flex-row gap-2">
             <select
