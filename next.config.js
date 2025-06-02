dbbbb0ad28de88674f5d5680dd90b793197f169e
gadji <135466448+Milkjf@users.noreply.github.com>
2025-06-02 04:04:48 +0000

    modification de mon site

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // ...autres options...
+  output: 'standalone',
+  // Pour les assets statiques
+  assetPrefix: '',
+  // Pour les chemins de base si besoin
+  // basePath: '',
+};
